a0c4d18237f712a38860f0252c31a6cda91fe979
Qin Zhou <1079207272@qq.com>
2022-02-15 16:26:39 +0800

    chore: update some api

--- packages/renderer/src/api/social.ts
+++ packages/renderer/src/api/social.ts
@@ -1,5 +1,18 @@
 import { request } from '../utils/request'
 
 export function login(data: { name: string; password: string }) {
-  return request.post('token', data)
+  return request.post('user/login', data)
+}
+
+export function getCaptcha(data: { email: string }) {
+  return request.post('user/captcha', data)
+}
+
+export function register(data: {
+  captcha: number
+  email: string
+  name: string
+  password: string
+}) {
+  return request.post('user/register', data)
 }
--- packages/renderer/src/utils/request.ts
+++ packages/renderer/src/utils/request.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const request = axios.create({
-  baseURL: 'http://phoenix.matrix53.top/',
+  baseURL: 'http://phoenix.matrix53.top/api/v1/',
 })
 
 export { request }
