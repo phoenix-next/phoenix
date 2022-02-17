118907f7bcb0cf00acb7132b6c79847d77eaad7c
Qin Zhou <1079207272@qq.com>
2022-02-17 19:11:41 +0800

    feat: add ts support for vue-router component

--- packages/renderer/src/global.d.ts
+++ packages/renderer/src/global.d.ts
@@ -1,5 +1,19 @@
+/// <reference types="vite/client" />
 
-export { }
+import vue from 'vue'
+declare module 'vue' {
+  export interface GlobalComponents {
+    RouterLink: typeof import('vue-router')['RouterLink']
+    RouterView: typeof import('vue-router')['RouterView']
+  }
+}
+
+declare module '*.vue' {
+  import { DefineComponent } from 'vue'
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any, @typescript-eslint/ban-types
+  const component: DefineComponent<{}, {}, any>
+  export default component
+}
 
 declare global {
   interface Window {
