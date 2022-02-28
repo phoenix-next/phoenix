8d5c766f1e2881868f1ef71fb00dbe94607997f9
Qin Zhou <1079207272@qq.com>
2022-02-28 21:53:30 +0800

    chore: update judge logic

--- packages/main/utils.ts
+++ packages/main/utils.ts
@@ -1,15 +1,16 @@
+import { join } from 'path'
+import { open, close, readFile } from 'fs-extra'
+import { spawn } from 'child_process'
 import { ipcMain, app } from 'electron'
 import MarkdownIt from 'markdown-it'
 import latex from 'markdown-it-texmath'
 import prism from 'markdown-it-prism'
-import path from 'path'
-import fs from 'fs-extra'
-import { spawn } from 'child_process'
+import { diffWords } from 'diff'
 
 const markdown = new MarkdownIt().use(latex).use(prism)
 const configPath = app.getPath('userData')
 const tmpPath = app.getPath('temp')
-const dataPath = path.join(configPath, 'data')
+const dataPath = join(configPath, 'data')
 
 export function handleUtils() {
   ipcMain.handle('markdownToHTML', (event, text) => {
@@ -27,10 +28,10 @@ export function handleUtils() {
   ipcMain.handle(
     'judgeProblem',
     async (event, srcFilePath, problemID, language) => {
-      const execPath = path.join(tmpPath, 'exec_' + problemID)
-      const outputPath = path.join(tmpPath, 'out_' + problemID)
-      const inputPath = path.join(dataPath, 'in_' + problemID)
-      const answerPath = path.join(dataPath, 'ans_' + problemID)
+      const execPath = join(tmpPath, 'exec_' + problemID)
+      const outputPath = join(tmpPath, 'out_' + problemID)
+      const inputPath = join(dataPath, 'in_' + problemID)
+      const answerPath = join(dataPath, 'ans_' + problemID)
       // compile and run
       switch (language) {
         case 'C':
@@ -38,11 +39,12 @@ export function handleUtils() {
           gcc.on('close', (code) => {
             console.log(code)
           })
-          const output = await fs.open(outputPath, 'w')
-          const input = await fs.open(inputPath, 'r')
-
+          const input = await open(inputPath, 'r')
+          const output = await open(outputPath, 'w')
           const run = spawn(execPath, [], { stdio: [input, output, 'pipe'] })
           run.on('close', (code) => {
+            close(input)
+            close(output)
             console.log(code)
           })
           break
@@ -58,12 +60,20 @@ export function handleUtils() {
           break
       }
       // compare output and ans
-      const compare = spawn('fc', ['/W', outputPath, answerPath], {
-        shell: true,
-      })
-      compare.on('close', (code) => {
-        console.log(code, 'haha')
-        // TODO: accept or wrong answer
+      const ansStr = await readFile(answerPath, 'utf8')
+      const outStr = await readFile(outputPath, 'utf8')
+      console.log(ansStr, outStr)
+      diffWords(ansStr, outStr, (error, change) => {
+        if (
+          !change ||
+          change.length !== 1 ||
+          change[0].added ||
+          change[0].removed
+        ) {
+          console.log('diff\n')
+        } else {
+          console.log('same\n')
+        }
       })
       // return result
       return 'pseudo judge result'
