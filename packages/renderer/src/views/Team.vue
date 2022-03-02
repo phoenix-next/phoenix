ca341902aaf1685932f01b9834e776b8bb327172
Kurino <gejingze@163.com>
2022-03-02 22:32:59 +0800

    feat: docking the organization API

--- packages/renderer/src/views/Team.vue
+++ packages/renderer/src/views/Team.vue
@@ -5,7 +5,7 @@
       <n-tab-pane name="team-list" tab="成员列表">
         <team-list
           :teamName="teamName"
-          :title-tooltip="isTeamManager ? '您当前是管理员' : ''"
+          :title-tooltip="isTeamAdmin ? '您当前是管理员' : ''"
         >
           <template #teamSelectList>
             <n-select
@@ -25,21 +25,81 @@
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue'
+import { onMounted, ref } from 'vue'
 import { useMessage } from 'naive-ui'
 import TeamList from '../components/team/TeamList.vue'
 import TeamSetting from '../components/team/TeamSetting.vue'
+import { getOrganization } from '../api/social'
+import { stringifyStyle } from '@vue/shared'
+import { SelectMixedOption } from 'naive-ui/lib/select/src/interface'
 
 const message = useMessage()
 
-// const organization
-const teamsNameRef = ref([{ label: '', value: '' }])
-const teamName = ref('')
+onMounted(
+  () => {
+    getOrganization().then(
+      (res: {
+        data: {
+          success: boolean
+          organization: {
+            id: number
+            isAdmin: boolean
+            name: string
+          }[]
+        }
+      }) => {
+        if (res.data.success) {
+          res.data.organization.forEach((element) => {
+            teamsNameRef.value.push({
+              label: element.name,
+              value: element.name
+            })
+            isTeamsAdminDic.set(element.name, element.isAdmin)
+            teamsIdDic.set(element.name, element.id)
+          })
+          teamName.value = teamsNameRef.value[0].label
+          isTeamAdmin.value = res.data.organization[0].isAdmin
+        }
+      }
+    )
+  }
+  // () => {
+  //   testTeamsData.data.organization.forEach((element) => {
+  //     teamsNameRef.value.push({ label: element.name, value: element.name })
+  //     isTeamsAdminDic.set(element.name, element.isAdmin)
+  //   })
+  //   teamName.value = teamsNameRef.value[0].label
+  //   isTeamAdmin.value = testTeamsData.data.organization[0].isAdmin
+  // }
+)
 
-const isTeamManager = ref(false)
+// const testTeamsData = {
+//   data: {
+//     success: true,
+//     organization: [
+//       {
+//         id: 1,
+//         name: 'kurino的队伍',
+//         isAdmin: true
+//       },
+//       {
+//         id: 2,
+//         name: 'phoneix-next',
+//         isAdmin: false
+//       }
+//     ]
+//   }
+// }
+
+const isTeamsAdminDic = new Map<string, boolean>()
+const teamsIdDic = new Map<string, number>()
+const teamsNameRef = ref<SelectMixedOption[]>([])
+const teamName = ref()
+const isTeamAdmin = ref<boolean | undefined>(false)
 const handleSelectTeam = (value: string) => {
   teamName.value = value
-  message.info('change to ' + value)
+  isTeamAdmin.value = isTeamsAdminDic.get(value)
+  message.info('Team change to ' + value)
 }
 </script>
 
