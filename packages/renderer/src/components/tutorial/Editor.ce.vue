18fb51b1e6a98454023da1fe27499a7f762d2715
Qin Zhou <1079207272@qq.com>
2022-03-11 15:25:17 +0800

    chore: prepare to add editor

---
+++ packages/renderer/src/components/tutorial/Editor.ce.vue
@@ -0,0 +1,7 @@
+<template>
+  <div style="height: 100px; width: 100px; background-color: aqua"></div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped></style>
--- packages/renderer/src/views/tutorial/TutorialDetail.vue
+++ packages/renderer/src/views/tutorial/TutorialDetail.vue
@@ -9,19 +9,22 @@
       <n-h1 class="title">{{ tutorial.name }}</n-h1>
     </n-space>
     <n-divider />
-    <div v-html="tutorial.detail"></div>
+    <div v-html="tutorial.detail" id="detail"></div>
   </n-card>
 </template>
 
 <script setup lang="ts">
-import { onMounted, ref, reactive } from 'vue'
+import { onMounted, ref, reactive, defineCustomElement } from 'vue'
 import { ArrowBackCircleOutline } from '@vicons/ionicons5'
 import { NCard, NButton, NIcon, NSpace, NH1, NDivider } from 'naive-ui'
 import { useRouter, useRoute } from 'vue-router'
 import { getTutorial } from '../../api/tutorial'
+import Editor from '../../components/tutorial/Editor.ce.vue'
 
 const router = useRouter()
 const route = useRoute()
+const CustomEditor = defineCustomElement(Editor)
+customElements.define('custom-editor', CustomEditor)
 
 const tutorial = reactive({
   detail: '<h2>前言</h2><h2>内容</h2><h2>结语</h2>',
@@ -46,6 +49,11 @@ onMounted(() => {
     .then((res) => {
       tutorial.detail = res
       pending.value = false
+    })
+    .then(() => {
+      const detail = document.getElementById('detail')
+      const editor = new CustomEditor()
+      detail?.appendChild(editor)
     })
 })
 </script>
