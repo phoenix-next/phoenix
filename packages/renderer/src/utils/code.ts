e9c20af381fce057b30125a808fa8c9c538dfa32
Qin Zhou <1079207272@qq.com>
2022-03-12 22:41:52 +0800

    feat: finish editor basic

--- packages/renderer/src/utils/code.ts
+++ packages/renderer/src/utils/code.ts
@@ -1,3 +1,6 @@
+import { Terminal } from 'xterm'
+import { FitAddon } from 'xterm-addon-fit'
+
 export function createEditor(container: HTMLElement, language: string) {
   const div = document.createElement('div')
   div.style.height = Math.max(container.offsetHeight, 110) + 'px'
@@ -52,6 +55,20 @@ export function addEditorAction(editor: monaco.editor.IStandaloneCodeEditor) {
     }
   })
   editor.addAction({
+    contextMenuGroupId: 'code',
+    label: 'Clear program output',
+    id: 'Clear program output',
+    run: (instance) => {
+      // 获取编辑器节点的父节点
+      const pre = instance.getDomNode()?.parentNode?.parentNode
+      // 删除已经挂载的xterm
+      if (pre?.children.length !== 1) {
+        pre?.removeChild(pre.lastChild as ChildNode)
+      }
+    }
+  })
+  editor.addAction({
+    contextMenuGroupId: 'code',
     label: 'Run code in editor',
     id: 'Run code in editor',
     run: (instance) => {
@@ -61,9 +78,36 @@ export function addEditorAction(editor: monaco.editor.IStandaloneCodeEditor) {
           instance.getModel()?.getLanguageId() || ''
         )
         .then((res) => {
-          console.log(res)
+          // 获取编辑器节点的父节点
+          const pre = instance.getDomNode()?.parentNode?.parentNode
+          // 将xterm挂载父节点下，无需重复挂载
+          if (pre?.children.length === 1) {
+            // 创建xterm的容器
+            const div = document.createElement('div')
+            div.style.height = '90px'
+            div.style.marginTop = '6px'
+            pre?.appendChild(div)
+            // 创建xterm的实例，并挂载
+            const terminal = new Terminal({
+              theme: {
+                background: '#f0f0f0',
+                foreground: '#000000',
+                cursor: '#888888'
+              }
+            })
+            const fitAddon = new FitAddon()
+            terminal.loadAddon(fitAddon)
+            ;(div as any).terminal = terminal
+            // 写入最新的程序输出内容
+            terminal.open(div)
+            terminal.write('output:\r\n' + res)
+            fitAddon.fit()
+          } else {
+            const terminal = (pre?.lastChild as any).terminal as Terminal
+            terminal.clear()
+            terminal.write('output:\r\n' + res)
+          }
         })
-      console.log(instance.getDomNode())
     }
   })
 }
