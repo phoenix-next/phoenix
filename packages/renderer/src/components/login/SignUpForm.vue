34bb89ba47d2aad29038914e3adf23b2ba4683a0
Qin Zhou <1079207272@qq.com>
2022-03-13 20:39:04 +0800

    chore: update backend api

--- packages/renderer/src/components/login/SignUpForm.vue
+++ packages/renderer/src/components/login/SignUpForm.vue
@@ -130,7 +130,7 @@ function clickGetCaptcha() {
     window.$message.warning('请先输入您的邮箱')
     return
   }
-  getCaptcha({ email: data.email }).then((res) => {
+  getCaptcha({ email: data.email, type: 1 }).then((res) => {
     if (res.data.success) {
       window.$message.success(res.data.message)
     }
