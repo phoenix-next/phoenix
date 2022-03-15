2cae9e8a751626625ce0456635d181b60cf04bf9
Kurino <gejingze@163.com>
2022-03-15 19:07:02 +0800

    chore: change team list UI

--- packages/renderer/src/components/team/TeamList.vue
+++ packages/renderer/src/components/team/TeamList.vue
@@ -8,100 +8,69 @@
   >
     新建组织
   </n-button>
-  <n-data-table
-    :columns="columns"
-    :data="data"
-    :loading="loading"
-    :pagination="pagination"
-    :row-key="rowKey"
-    @update:page="handlePageChange"
-  />
+
+  <n-scrollbar style="max-height: 600px">
+    <div v-for="(item, index) in data">
+      <n-card>
+        <n-thing>
+          <template #avatar>{{ 'O' + item.orgID }}</template>
+          <template #header>
+            {{ item.orgName }}
+            <n-button
+              size="tiny"
+              secondary
+              round
+              :focusable="false"
+              v-if="item.isAdmin"
+            >
+              管理员
+            </n-button>
+          </template>
+          <template #header-extra>
+            <n-button
+              size="small"
+              secondary
+              round
+              type="info"
+              @click="handleClick(item.orgID)"
+            >
+              详细信息
+            </n-button>
+          </template>
+        </n-thing>
+      </n-card>
+      <n-divider v-if="index != teamsNum - 1"></n-divider>
+    </div>
+  </n-scrollbar>
   <team-add ref="teamAdd" />
 </template>
 
 <script setup lang="tsx">
-import { NDataTable, NButton, DataTableColumn } from 'naive-ui'
+import { NButton, NThing, NScrollbar, NCard, NDivider } from 'naive-ui'
+import { ref, onMounted } from 'vue'
 import TeamAdd from './modal/TeamAdd.vue'
-import { ref, reactive, onMounted } from 'vue'
 import { getUserOrganization } from '../../api/user'
 import { useRouter } from 'vue-router'
 
 const router = useRouter()
 
-const loading = ref(true)
-const data = ref([])
 const teamAdd = ref<InstanceType<typeof TeamAdd> | null>(null)
-const columns = ref<Array<DataTableColumn>>([
-  {
-    title: '组织编号',
-    key: 'orgID',
-    width: '250px',
-    render: (rowData) => {
-      return (
-        <div
-          onClick={handleClick(rowData.orgID as number)}
-          style={{ cursor: 'pointer' }}
-        >
-          {'O' + rowData.orgID}
-        </div>
-      )
-    }
-  },
-  {
-    title: '组织名称',
-    key: 'orgName',
-    render: (rowData) => {
-      return (
-        <div
-          onClick={handleClick(rowData.orgID as number)}
-          style={{ cursor: 'pointer' }}
-        >
-          {rowData.orgName}
-        </div>
-      )
-    }
-  },
-  {
-    title: '身份',
-    key: 'isAdmin',
-    width: '200px',
-    render: (rowData) => {
-      return (
-        <div
-          onClick={handleClick(rowData.orgID as number)}
-          style={{ cursor: 'pointer' }}
-        >
-          {rowData.isAdmin ? '管理员' : '组员'}
-        </div>
-      )
-    }
-  }
-])
-const pagination = reactive({
-  page: 1,
-  itemCount: 1,
-  pageSize: 10
-})
+type teamInfo = { isAdmin: boolean; orgID: number; orgName: string }
+const data = ref<Array<teamInfo>>()
+const teamsNum = ref(0)
+const loading = ref(false)
 
 function handleCreate() {
   teamAdd.value?.open()
 }
 function handleClick(id: number) {
-  return () => {
-    router.push({ path: '/team/' + id })
-  }
-}
-function handlePageChange(currentPage: number) {
-  pagination.page = currentPage
-}
-function rowKey(rowData: any) {
-  return rowData.orgID
+  router.push({ path: '/team/' + id })
 }
 
 onMounted(() => {
   getUserOrganization().then((res) => {
     data.value = res.data.organization
-    pagination.itemCount = res.data.organization.length
+    teamsNum.value = res.data.organization.length
     loading.value = false
   })
 })
