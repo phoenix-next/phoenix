3cce81f4143dd4dc0ab252fa42d3d1c0b714aceb
Qin Zhou <1079207272@qq.com>
2022-03-29 09:02:58 +0800

    chore: update contest detail

--- packages/renderer/src/views/contest/ContestDetail.vue
+++ packages/renderer/src/views/contest/ContestDetail.vue
@@ -1,7 +1,7 @@
 <template>
   <n-layout>
     <n-layout-header>
-      <div class="center">
+      <div class="center fill">
         <n-card class="header">
           <n-h1 style="margin-bottom: 3px">{{ data.name }}</n-h1>
           <n-tabs :value="tab" @update-value="handleUpdate">
@@ -13,7 +13,7 @@
     </n-layout-header>
     <n-layout has-sider sider-placement="right">
       <n-layout-content>
-        <div class="center">
+        <div class="center fill">
           <n-card class="content" v-if="tab === 'profile'">
             <n-p>开始时间：{{ data.startTime }}</n-p>
             <n-p>结束时间：{{ data.endTime }}</n-p>
@@ -23,14 +23,38 @@
             <template v-for="item in problemList">
               <n-card
                 @click="enterProblem(item.problemID)"
-                style="cursor: pointer"
+                style="cursor: pointer; margin-top: 4px"
               >
                 <n-grid>
-                  <n-grid-item span="15">{{ item.problemName }}</n-grid-item>
-                  <n-grid-item span="7">
-                    难度：{{ item.difficulty }}
+                  <n-grid-item span="2" class="center">
+                    <NIcon size="20" color="#626262" v-if="item.result === 0">
+                      <RemoveSharp />
+                    </NIcon>
+                    <NIcon
+                      size="20"
+                      color="#0e7a0d"
+                      v-else-if="item.result > 0"
+                    >
+                      <CheckmarkDoneOutline />
+                    </NIcon>
+                    <NIcon size="20" color="#FF3939" v-else>
+                      <Close />
+                    </NIcon>
                   </n-grid-item>
-                  <n-grid-item span="2">T{{ item.problemID }}</n-grid-item>
+                  <n-grid-item span="10" class="center">
+                    {{ item.problemName }}
+                  </n-grid-item>
+                  <n-grid-item span="2">难度：</n-grid-item>
+                  <n-grid-item span="7" class="center">
+                    <NProgress
+                      :percentage="(item.difficulty as number) * 10"
+                      :showIndicator="false"
+                      :status="getStatus(item.difficulty)"
+                    />
+                  </n-grid-item>
+                  <n-grid-item span="2" offset="1" class="center">
+                    T{{ item.problemID }}
+                  </n-grid-item>
                 </n-grid>
               </n-card>
             </template>
@@ -38,7 +62,7 @@
         </div>
       </n-layout-content>
       <n-layout-sider width="400">
-        <div class="center">
+        <div class="center fill">
           <n-card class="sider">
             <n-h2>排行榜</n-h2>
           </n-card>
@@ -51,6 +75,7 @@
 <script setup lang="ts">
 import { useRoute, useRouter } from 'vue-router'
 import { onMounted, ref, reactive } from 'vue'
+import { CheckmarkDoneOutline, Close, RemoveSharp } from '@vicons/ionicons5'
 import {
   NCard,
   NTabs,
@@ -63,7 +88,9 @@ import {
   NH2,
   NP,
   NGrid,
-  NGridItem
+  NGridItem,
+  NProgress,
+  NIcon
 } from 'naive-ui'
 import { getContest } from '../../api/contest'
 
@@ -84,6 +111,11 @@ function handleUpdate(value: any) {
 function enterProblem(id: number) {
   router.push(`/problem/${id}`)
 }
+function getStatus(difficulty: any) {
+  if ((difficulty as number) > 8) return 'error'
+  else if ((difficulty as number) > 5) return 'warning'
+  else if ((difficulty as number) > 3) return 'success'
+}
 
 onMounted(() => {
   getContest({ id: route.params.id as string }).then((res) => {
@@ -98,11 +130,14 @@ onMounted(() => {
 
 <style scoped>
 .center {
-  height: 100%;
-  width: 100%;
   display: flex;
   justify-content: center;
   align-items: center;
+}
+
+.fill {
+  height: 100%;
+  width: 100%;
 }
 .header:deep(.n-card__content) {
   padding-bottom: 0;
